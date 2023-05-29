7c6ae3bc9882d1831dd5c698809c23a8c7037ae9
krsnamara <martinj.fitzpatrick@gmail.com>
2023-05-29 09:36:55 -0400

    building out repo

---
+++ server/s3.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3Client = new S3Client({
+  region,
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
+
+// Uploads a file to S3
+async function uploadFile(file) {
+  const fileStream = fs.createReadStream(file.path);
+
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename,
+  };
+
+  const command = new PutObjectCommand(uploadParams);
+  return s3Client.send(command);
+}
+
+exports.uploadFile = uploadFile;
+
+// Downloads a file from S3
+function getFileStream(fileKey) {
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName,
+  };
+
+  return s3Client.getObject(downloadParams).createReadStream();
+}
+
+exports.getFileStream = getFileStream;
