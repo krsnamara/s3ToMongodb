b9427c92527b6641132db3055d4258b64207b118
krsnamara <martinj.fitzpatrick@gmail.com>
2023-06-05 14:21:41 -0400

    initializing prisma and building out the schema

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 import express from 'express'
-
 import multer from 'multer'
 // import sharp from 'sharp'
 // import crypto from 'crypto'
 
 import { PrismaClient } from '@prisma/client'
-import { uploadFile, deleteFile, getObjectSignedUrl } from './s3.js'
+
+import { S3Client } from '@aws-sdk/client-s3';
+
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const bucketRegion = process.env.AWS_BUCKET_REGION
+const accessKey = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+
+const s3 = new S3Client ({
+  credentials: {
+  accessKeyId: accessKey,
+  secretAccessKey: secretAccessKey,
+  },
+  region: bucketRegion
+}); 
 
 const app = express()
 const prisma = new PrismaClient()
@@ -13,46 +30,22 @@ const prisma = new PrismaClient()
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
-const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
-
 app.get("/api/posts", async (req, res) => {
   const posts = await prisma.posts.findMany({orderBy: [{ created: 'desc'}]})
-  for (let post of posts) {
-    post.imageUrl = await getObjectSignedUrl(post.imageName)
-  }
   res.send(posts)
 })
 
 
 app.post('/api/posts', upload.single('image'), async (req, res) => {
-  const file = req.file
-  const caption = req.body.caption
-  const imageName = generateFileName()
-
-  const fileBuffer = await sharp(file.buffer)
-    .resize({ height: 1920, width: 1080, fit: "contain" })
-    .toBuffer()
-
-  await uploadFile(fileBuffer, imageName, file.mimetype)
-
-  const post = await prisma.posts.create({
-    data: {
-      imageName,
-      caption,
-    }
-  })
-  
-  res.status(201).send(post)
+  console.log("req.body", req.body)
+  console.log("req.file", req.file)
+  req.file.buffer
+  res.send({})
 })
 
 app.delete("/api/posts/:id", async (req, res) => {
   const id = +req.params.id
-  const post = await prisma.posts.findUnique({where: {id}}) 
-
-  await deleteFile(post.imageName)
-
-  await prisma.posts.delete({where: {id: post.id}})
-  res.send(post)
+  res.send({})
 })
 
 app.listen(8080, () => console.log("listening on port 8080"))
